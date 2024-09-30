9d6887829e53874e2906df9a821cf0dbbf3eb300
ihab abadi <ihab@utopios.net>
2024-09-30 16:23:41 +0200

    provider openstack

---
+++ demo_provider/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    openstack = {
+        source = "terraform-provider-openstack/openstack"
+        version = "1.53.0"
+    }
+  }
+}
+provider "openstack" {
+  user_name   = "admin"
+  tenant_name = "admin"
+  password    = "FormationTerraform1234."
+  auth_url    = "https://5.135.138.197:5000/v3/"
+  region      = "microstack"
+  insecure = true
+}
+
